f6ab3a87d7e5f39fd235cd474d3bffa9ff746e16
u012990751 <2458853753@qq.com>
2014-07-17 11:33:01 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=nrwxIqtCaowxScVj6K6Yk6mQ"></script>
 AV.Cloud.define("hello", function(request, response) {
 	response.success("测试成功");
 });
