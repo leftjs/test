8305e7caabca1f3e20ca02b55ef11012277d3e81
u012990751 <2458853753@qq.com>
2014-07-17 11:25:51 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,49 @@
-// Use AV.Cloud.define to define as many cloud functions as you want.
-// For example:
+<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=nrwxIqtCaowxScVj6K6Yk6mQ"></script>
 AV.Cloud.define("hello", function(request, response) {
-  response.success("测试成功");
+	response.success("测试成功");
 });
+AV.Cloud.define("getItem", function(request, response) {
+	var items = new Array();
+	var query = new AV.Query("Item");
+	query.equalTo("ciry", request.params.city);
+	query.find({
+		success : function(results) {
+		//	for (var i = 0; i < results.length; ++i) {
+		//		if()
+		//	}
+			response.success(getFlatternDistance(0,0,0,0));
+		},
+		error : function() {
+			response.error("获取失败");
+		}
+	});
+});
+
+function getFlatternDistance(lat1,lng1,lat2,lng2){ 
+var f = getRad((lat1 + lat2)/2); 
+var g = getRad((lat1 - lat2)/2); 
+var l = getRad((lng1 - lng2)/2); 
+
+var sg = Math.sin(g); 
+var sl = Math.sin(l); 
+var sf = Math.sin(f); 
+
+var s,c,w,r,d,h1,h2; 
+var a = EARTH_RADIUS; 
+var fl = 1/298.257; 
+
+sg = sg*sg; 
+sl = sl*sl; 
+sf = sf*sf; 
+
+s = sg*(1-sl) + (1-sf)*sl; 
+c = (1-sg)*(1-sl) + sf*sl; 
+
+w = Math.atan(Math.sqrt(s/c)); 
+r = Math.sqrt(s*c)/w; 
+d = 2*w*a; 
+h1 = (3*r -1)/2/c; 
+h2 = (3*r +1)/2/s; 
+
+return d*(1 + fl*(h1*sf*(1-sg) - h2*(1-sf)*sg)); 
+}
