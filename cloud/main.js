e21b3e20e3045a258c710cf7956c985fb1a7bb84
u012990751 <2458853753@qq.com>
2014-07-26 20:33:20 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -56,11 +56,10 @@ AV.Cloud.define("getFavorite", function(request, response) {
 AV.Cloud.define("getShoppingCart", function(request, response) {
 	var Item = AV.Object.extend("Item");
 	var query = new AV.Query(Item);
-	response.success(request.params.itemIds.length);
 	query.containedIn("objectId", request.params.itemIds);
 	query.find({
 		success : function(results) {
-			response.success(results);
+			response.success(results.length);
 		},
 		error : function(error) {
 			response.error(error.message);
