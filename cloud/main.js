e06c2698dd4cd97fcb7464eb2758e6456370cc49
u012990751 <2458853753@qq.com>
2014-07-26 09:59:22 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -19,3 +19,57 @@ AV.Cloud.define("getItem", function(request, response) {
 		}
 	});
 });
+//获取收藏
+AV.Cloud.define("getFavorite", function(request, response) {
+	var favoriateItem = AV.Object.extend("Favorite");
+	var query = new AV.Query(favoriateItem);
+	query.equalTo("userId", request.params.userId);
+	query.find({
+		success : function(results) {
+			if (results.length == 0) {
+				response.error("没有收藏");
+			} else {
+				var favoriates = new Array();
+				for ( i = 0; i < results.length; i++) {
+					favoriates.push(results.get("itemId"));
+				}
+				var Item = AV.Object.extend("Item");
+				var query = new AV.Query(Item);
+				query.containedIn("objectId", favoriates);
+				query.find({
+					success : function(results) {
+						response.success(results);
+					},
+					error : function(error) {
+						response.error(error.message);
+					}
+				});
+			}
+		},
+		error : function(error) {
+			response.error(error.message);
+		}
+	});
+});
+//下单处理函数
+AV.Cloud.define("placeOrder", function(request, response) {
+	var Order = AV.Object.extend("Order");
+	var order = new Order();
+	order.set("items", request.params.items);
+	// User's location
+	var location = new AV.GeoPoint({
+		latitude : request.params.latitude,
+		longitude : request.params.longitude
+	});
+	order.set("location", location);
+	order.set("userId", request.params.userId);
+	order.set("state", 1);
+	order.save(null, {
+		success : function(order) {
+			response.success();
+		},
+		error : function(order, error) {
+			response.error(error.description);
+		}
+	});
+});
--- public/logic.js
+++ public/logic.js
@@ -1,5 +1,10 @@
-//加载商品
-function loadItem(Items) {
+//popup对话框对象
+var popup;
+//购物车集合
+var shoppoingCart = new Array();
+
+//加载主页商品
+function loadItemMainPanel(Items) {
 	var geo = new AV.GeoPoint({
 		latitude : 31.717531,
 		longitude : 118.787853
@@ -19,7 +24,6 @@ function loadItem(Items) {
 		var $li = $("<li></li>");
 		var $a = $("<a></a>");
 		$a.append($img, $div);
-		//var test =
 		var setItem = "localStorage.setItem('itemId', " + "'" + Items[i].objectId + "'" + ")";
 		$a.attr('onclick', setItem);
 		$a.attr("href", "#buy_panel");
@@ -29,6 +33,7 @@ function loadItem(Items) {
 	}
 }
 
+//初始化广告
 function initAdvertise() {
 	var ad = AV.Object.extend("Advertise");
 	var query = new AV.Query(ad);
@@ -49,6 +54,7 @@ function initAdvertise() {
 	});
 }
 
+//加载购买面板
 function buyPanelLoad() {
 	var imageUrl;
 	var description;
@@ -70,12 +76,13 @@ function buyPanelLoad() {
 	});
 }
 
+//收藏
 function collect() {
 	var currentUser = AV.User.current();
 	if (currentUser) {
-		var Favoraite = AV.Object.extend("Favoraite");
+		var Favoraite = AV.Object.extend("Favorite");
 		var favoraite = new Favoraite();
-		favoraite.set("userId", currentUser.get("objectId"));
+		favoraite.set("userId", currentUser.id);
 		favoraite.set("itemId", localStorage.getItem('itemId'));
 		favoraite.save(null, {
 			success : function(favoraite) {
@@ -90,11 +97,130 @@ function collect() {
 		});
 	} else {
 		// show the signup or login page
-		loginPop();
+		popup = loginPop();
+
 	}
 }
 
+//加入购物车
+function addToShoppingCart() {
+	var currentUser = AV.User.current();
+	if (currentUser) {
+		shoppoingCart.push(localStorage.getItem('itemId'));
+	} else {
+		// show the signup or login page
+		popup = loginPop();
+	}
+}
+
+//登录框
 function loginPop() {
-	af("#afui").popup("I'm replacing an alert box");
+	return af("#afui").popup({
+		title : "用户登录",
+		message : "手机号: <input id='phone_number_login' type='text' class='af-ui-forms'><br />密码: <input id='password_login' type='text' class='af-ui-forms' style='webkit-text-security:disc'><br />还没有账号？请<span style='font-size:25px;color:blue;' onclick='registerPop()'>点击注册</span>",
+		cancelText : "取消",
+		cancelCallback : function() {
+		},
+		doneText : "登录",
+		doneCallback : function() {
+			AV.User.logIn($("#phone_number_login").val(), $("#password_login").val(), {
+				success : function(user) {
+					// Do stuff after successful login.
+					alert("成功");
+				},
+				error : function(user, error) {
+					// The login failed. Check error to see why.
+					alert(error.message);
+				}
+			});
+		},
+		cancelOnly : false
+	});
+}
+
+//注册框
+function registerPop() {
+	popup.hide();
+	popup = af("#afui").popup({
+		title : "用户注册",
+		message : "手机号: <input id='phone_number_register' type='text' class='af-ui-forms'><br />密码: <input id='password_register' type='text' class='af-ui-forms' style='webkit-text-security:disc'><br />确认密码: <input id='password_confirm_register'  type='text' class='af-ui-forms' style='webkit-text-security:disc'>",
+		cancelText : "取消",
+		cancelCallback : function() {
+		},
+		doneText : "注册",
+		doneCallback : function() {
+			//处理注册的一些操作
+			if (!$('#phone_number_register').val().match(/^1[3|4|5|8][0-9]\d{4,8}$/)) {
+				alert("手机号码格式不正确");
+			} else if ($('#password_register').val() != $('#password_confirm_register').val()) {
+				alert("两次密码输入不一致");
+			} else if (($('#password_register').val().length < 6) || ($('#password_register').val().length > 20)) {
+				alert("密码长度请保持在6到20位之间");
+			} else {
+				var user = new AV.User();
+				user.set("username", $('#phone_number_register').val());
+				user.set("password", $('#password_register').val());
+				user.set("numberVerified", false);
+				// 更新地理位置信息
+				//	user.set("location", Geolocation);
+				//	user.set("adress", Adress);
+
+				user.signUp(null, {
+					success : function(user) {
+						// Hooray! Let them use the app now.
+						alert("注册成功");
+					},
+					error : function(user, error) {
+						// Show the error message somewhere and let the user try again.
+						alert("失败: " + error.code + " " + error.message);
+					}
+				});
+			}
+		},
+		cancelOnly : false
+	});
+}
+
+//加载收藏商品
+function CollectPanelLoad() {
+	var currentUser = AV.User.current();
+	if (currentUser) {
+		AV.Cloud.run('getFavorite', {
+			"userId" : currentUser.id
+		}, {
+			success : function(result) {
+				alert(1);
+			},
+			error : function(error) {
+				alert(error.message);
+			}
+		});
+	} else {
+		$("#collect_panel ul").remove();
+		if (!$('#collect_panel #no_collect_tip_img_collect_panel').attr('src')) {
+			var img = "<img id='no_collect_tip_img_collect_panel' src='images/no_collect.png' />";
+			$("#collect_panel > div").append(img);
+		}
+	}
+}
+
+function placeOrder() {
+	if (shoppoingCart.length == 0) {
+		af("#afui").popup("您的购物车内没有任何物品");
+	} else {
+		AV.Cloud.run('placeOrder', {
+			"latitude" : 31.717531,
+			"longitude" : 118.787853,
+			"userId" : AV.User.current().id,
+			"items" : shoppoingCart
+		}, {
+			success : function(result) {
+				af("#afui").popup("下单成功，请等待送货");
+			},
+			error : function(error) {
+				alert(error.message);
+			}
+		});
+	}
 }
 
