0682300b607d7a4ebcb546308fa869c135c0e93f
u012990751 <2458853753@qq.com>
2014-07-31 17:24:18 +0800

    test

--- cloud/main.js
+++ cloud/main.js
@@ -14,7 +14,6 @@ AV.Cloud.define("getItem", function(request, response) {
 		if (shops.length == 0) {
 			response.error("失败");
 		} else {
-			response.success(shops[0]);
 			var shopIds = new Array();
 			for (var i = 0; i < shops.length; i++) {
 				shopIds.push(shops[i].get("objectId"));
